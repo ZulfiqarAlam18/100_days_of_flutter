67b2dc7c472ef9ea98a8ce9793e241c056dc6da3
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-10-02 16:48:27 +0500

    sql setup and file added

---
+++ databases_with_flutter/lib/db_helper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ToDoDataBase extends StatefulWidget{
+  @override
+  String? id;
+  String? title;
+  String? des;
+  String? date ;
+  String? category;
+
+
+
+
+}
