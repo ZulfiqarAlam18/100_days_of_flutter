f6e1bfc7ca0d725d89ab36c97be773152234086d
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-10-03 17:43:02 +0500

    initail stage of project

--- databases_with_flutter/android/app/src/main/kotlin/com/example/databases_with_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.databases_with_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ databases_with_flutter_sqlite/android/app/src/main/kotlin/com/example/databases_with_flutter_sqlite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.databases_with_flutter_sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
