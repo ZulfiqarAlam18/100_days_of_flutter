7efd974af8da6bf7972f20d3864ff3755b87569f
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-01-19 15:32:03 +0500

    logic building day 1

---
+++ dart_basics/dart_practice.dart
@@ -0,0 +1,198 @@
+// dart basics
+import 'dart:io';
+
+
+
+// 01
+
+void main() {
+  
+  // int a = 10;
+  
+  // int b = 20;
+  
+  // print(a+b);
+  
+  
+  // print("Hello World");
+  practice_1();
+  
+  
+}
+
+
+// void practice_0(){
+  
+  
+//   print("Let's start");
+  
+// }
+
+void practice_1(){
+  
+  
+  /*
+  
+  Create variables to store:
+
+Your age
+
+Your height (decimal)
+
+Your name
+
+Whether you are a Flutter developer
+
+
+*/
+
+  // String name = "Zulfiqar";
+  
+  // int age = 23;
+  
+  // double height = 5.9;
+  
+  // bool isFlutterDev = true;
+  
+  
+  // age = 20;
+  // var age1 = 20;
+  
+  // final age2 = 30;
+  
+  // const age3 = 40;
+  
+  
+  
+  // 02
+  
+    //String? n1 = stdin.readLineSync(); // taking string in dart
+
+  
+ 
+  
+  // print('Enter first number :');
+  // int num1 = int.parse(stdin.readLineSync()!);
+  // print('Enter Second number :');
+  // int num2 = int.parse(stdin.readLineSync()!);
+  // print('Enter Operator :');
+  // String? op = stdin.readLineSync();
+ 
+ 
+ 
+  
+  // if(op == '/'){
+    
+  //   print(num1/num2);
+    
+  // }
+  // else if (op == "+"){
+    
+    
+  //   print(num1+num2);
+    
+    
+  // }
+  // else if (op == "-"){
+    
+    
+  //   print(num1-num2);
+    
+    
+  // }
+  // else if (op == "*"){
+    
+    
+  //   print(num1 * num2);
+    
+    
+  // }
+  // else {
+    
+    
+  //   print("Invaild Operator");
+    
+  // }
+  
+  // 03
+  
+  // int a = 10;
+  
+  // double b = 3;
+  
+  // int result  = a~/b; // integer devision
+  
+  // print(a+b);
+  // print(a/b);
+  // print(a%b);
+  // print(result);
+  
+  // 04
+  
+  // var number = 30;
+  
+  
+  // number = "30";
+  
+  // print(number);
+
+
+ // 05
+  // print("Enter width of plot:");
+  // int widh = int.parse(stdin.readLineSync()!);
+  // print("Enter height of plot:");
+  // int height = int.parse(stdin.readLineSync()!);
+  
+  
+  // int area = widh * height ;
+  
+  // if (area > 500) {
+    
+    
+  //   print('Large plot with area ${area}');
+    
+  // }
+  // else if (area > 100 && area < 500) {
+    
+  //       print('Medium plot with area ${area}');
+
+    
+  // }
+  // else{
+  //       print('Small plot with area ${area}');
+
+    
+    
+  // }
+  
+  // 06
+  
+  num height = 5; // num datatype is parent datatype of double and int
+  
+  height = 5.5;
+  
+  print(height);
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+}
+
+
