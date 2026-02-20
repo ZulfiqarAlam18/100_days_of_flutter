7ab81e454205219c6ba39afd25275f1a071dc079
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-02-20 20:43:59 +0500

    Word reversing in a string

--- 30_practice_problems/reversing_str.dart
+++ 30_practice_problems/reversing_str.dart
@@ -1,16 +1,66 @@
-void main(){
-
-
-  String str = "Hello World";
+void main() {
+  String str = "Hello World Zulfiqar";
   String reversedStr = reverseString(str);
-  print(reversedStr); // Output: "dlroW olleH"
+  //print(reversedStr);
+  //print(practice(str)); // 'Output: "dlroW olleH"
+  //print(reverseStringUsingSplit(str));
 
+  List<String> list = ['Jamil', 'Asad'];
 
+ // print(list.reversed.toString());
+
+  print(demo(str));
 }
+
 String reverseString(String str) {
   String reversed = '';
   for (int i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
   }
+  return reversed;
+}
+
+String reverseStringUsingSplit(String str) {
+  List<String> charList = str.split('');
+  List<String> reversedList = charList.reversed.toList();
+  return reversedList.join('');
+}
+
+String practice(String str) {
+  String reversed = "";
+
+  for (int i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+
+String demo(String str) {
+  String reversed = '';
+
+  List<String> list = [];
+  String temp = '';
+
+  for (int i = 0; i <= str.length - 1; i++) {
+    if (str[i] != ' ') {
+      temp += str[i];
+    } 
+    else{
+
+      list.add(temp);
+
+      temp = '';
+
+
+
+    }
+
+  }
+  list.add(temp);
+
+
+  reversed = list.reversed.toString();
+
   return reversed;
 }
