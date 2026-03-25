cd1cfe0226ec5e248d02934e5a872536bab2881a
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-03-25 12:17:57 +0500

    day 2

---
+++ restart/cal.dart
@@ -0,0 +1,20 @@
+void main(){
+
+
+  cal(20,'+',2);
+
+
+}
+
+void cal(int num1, String op , int num2){
+
+
+  if (op == '+'){
+
+    print('THE  SUM OF $num1 AND $num2 IS ${num1+num2}');
+
+  }
+
+
+
+}
