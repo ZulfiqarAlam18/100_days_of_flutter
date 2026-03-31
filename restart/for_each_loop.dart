ecca3ada04d13e9421cdd06f5112a45340a0fd77
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2026-03-31 09:09:52 +0500

    loop practice

---
+++ restart/for_each_loop.dart
@@ -0,0 +1,19 @@
+void main(){
+
+
+    List <int> marks = [80,90,99,77];
+
+    int i = 1;
+
+
+    for (int mark in marks){
+
+
+        print("Subject ${i} :  ${mark}");
+
+        i++;
+    }
+
+
+
+}
